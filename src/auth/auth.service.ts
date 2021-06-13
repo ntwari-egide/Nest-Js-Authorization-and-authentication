41f6f8bc38875bcb8aa0e0f51192d545586836f8
yombi <ntwariegide2@gmail.com>
2021-06-13 23:27:43 +0200

    Initializing prject dependencies

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService,private jwtService: JwtService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return resul
